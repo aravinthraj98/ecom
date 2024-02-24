f74ab3532e0541143d09b2cb1d340ac69d489f84
aravinthraj98 <aravinth799902@gmail.com>
2024-02-24 19:57:50 +0530

    initial commit

---
+++ src/app/grid/grid-container/grid-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './grid-container.component.html',
+  styleUrl: './grid-container.component.scss'
+})
+export class GridContainerComponent {
+
+}
---
+++ src/app/grid/grid-items/grid-items.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-items',
+  standalone: true,
+  imports: [],
+  templateUrl: './grid-items.component.html',
+  styleUrl: './grid-items.component.scss'
+})
+export class GridItemsComponent {
+
+}
